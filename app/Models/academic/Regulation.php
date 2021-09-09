c64bd4f56cd0345e15881bb43ebb7f514de92180
mohanavula <mohanavula@yahoo.com>
2021-09-09 14:29:08 +0530

    Initial commit

---
+++ app/Models/academic/Regulation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\academic;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Regulation extends Model
+{
+    use HasFactory;
+    protected $connection = 'ksrm';
+}
